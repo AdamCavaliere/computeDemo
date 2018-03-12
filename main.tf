26339803f73e83d5b49d65e8dea707288c728fa3
AdamCavaliere <adam.cavaliere@gmail.com>
2018-03-12 14:00:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "foo" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${lookup(var.avail_zone, count.index)}"
-  count = 3
+  count = 2
   tags {
     owner = "Adam"
     TTL = 5
